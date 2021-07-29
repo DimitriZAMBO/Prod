723dc73c2e012e94c9573dd48812ac0a7cb30665
DimitriZAMBO <dimitri.dieuiseul@gmail.com>
2021-07-29 11:32:47 +0200

    projet Prod

---
+++ src/Entity/Artiste.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArtisteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArtisteRepository::class)
+ */
+class Artiste
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $age;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $instrument;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getRegistre(): ?string
+    {
+        return $this->registre;
+    }
+
+    public function setRegistre(string $registre): self
+    {
+        $this->registre = $registre;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getInstrument(): ?string
+    {
+        return $this->instrument;
+    }
+
+    public function setInstrument(string $instrument): self
+    {
+        $this->instrument = $instrument;
+
+        return $this;
+    }
+}
